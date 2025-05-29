cf65cbdee3d38f3b7cf329a12c18172560e4e367
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2025-05-29 18:18:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/food-Delivery-Website/',
+  base: '/food-Delivery-Website/',  // Make sure this matches your repo name
   plugins: [react()],
-})
+});
